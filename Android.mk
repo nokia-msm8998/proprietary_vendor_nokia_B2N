b35fdb47da18d32e88eef08cd12e5a5849be9c3e
Tuan Anh <tuan73176@gmail.com>
2025-03-24 22:18:15 +0700

    B2N: Initial proprietary vendor blobs
    Change-Id: Ieff22ba560d7bcf649a559b7d5c5d3431ccb900b

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),B2N)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,f91c4c77446cd4b4f2382ae8bd0ef15492ab0737)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,ef62b0878ce1338b97d9324a3f642b4673d3127e)
+$(call add-radio-file-sha1-checked,radio/cda.img,64e2689827f65ffe8060b24f96710a20b3dbca9d)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,e11f3ab17136a86387a9247f8b7d684c3cc29496)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,6d2d25046b8f43bb3678d7838d1b5e65ed8beaf5)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,09ee2a91a67868c7c158333beb000e25880a360f)
+$(call add-radio-file-sha1-checked,radio/dsp.img,2170d1f2d838d41d352d826ec872e3a41f94aab6)
+$(call add-radio-file-sha1-checked,radio/hidden.img,9f7103658b0209b74137d6cb7e39986e54b03c04)
+$(call add-radio-file-sha1-checked,radio/hyp.img,ac6907e475d34d6979359c214d1f26a4e9550b0f)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,c1a52b82df8576e843c70d384e48414547724e5c)
+$(call add-radio-file-sha1-checked,radio/mdtp.img,b186389906aa96087fe3b6e0f2bcc1d4054bd02d)
+$(call add-radio-file-sha1-checked,radio/mdtpsecapp.img,3a418e29bb4d94220aadb6d42a963d4487214513)
+$(call add-radio-file-sha1-checked,radio/modem.img,73b2e6b6225eda947c0ce349465f1806189aec5c)
+$(call add-radio-file-sha1-checked,radio/nvdef.img,87685bb4c02e19cdc226b7ea7382c52aabf5862d)
+$(call add-radio-file-sha1-checked,radio/pmic.img,23803b25ca4a6edf0b2ba4669a45320ae6682b12)
+$(call add-radio-file-sha1-checked,radio/rpm.img,1e5fe23d3d59abcaf6cd1d62382eb04a934396f8)
+$(call add-radio-file-sha1-checked,radio/splash.img,533e16f1bc33fbc81c95c7c3cf7022c12d7dd377)
+$(call add-radio-file-sha1-checked,radio/systeminfo.img,566d6aa26509893751a98b37f02ad4887c8440cb)
+$(call add-radio-file-sha1-checked,radio/tz.img,857d490e55ce57ae9297bf1397422927bb9298c2)
+$(call add-radio-file-sha1-checked,radio/xbl.img,b6701a994bc8958fe38b0585f3e80ebeb32a7dad)
+
+endif
